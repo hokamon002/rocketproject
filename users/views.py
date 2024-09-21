04796883d17d96903e954b4e19251ec280d0acc9
Andy Tran <andytran0x002@gmail.com>
2024-09-21 13:33:28 -0700

    user login routes

--- users/views.py
+++ users/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from django.shortcuts import render, redirect 
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm 
+from django.contrib.auth import login, logout
+
+def register_view(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            login(request, form.save())
+            return redirect("")
+    else:
+        form = UserCreationForm()
+    return render(request, "users/register.html", {"form": form}) 
+
+def login_view(request):
+    return HttpResponse(b"Hello, world. You're at the polls index.")
+
+def logout_view(request):
+    return HttpResponse(b"Hello, world. You're at the polls index.")
