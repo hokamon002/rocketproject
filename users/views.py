9a9da2f1e40ca1f09bd65837f9693d9211c4e48d
Andy Tran <andytran0x002@gmail.com>
2024-09-22 12:19:08 -0700

    register with team

--- users/views.py
+++ users/views.py
@@ -4,14 +4,25 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, logout
 from .forms import TeamRegistrationForm
+from .models import Student, Team
 
 
 def register_view(request):
     if request.method == "POST":
         form = TeamRegistrationForm(request.POST)
         if form.is_valid():
+            user = form.save()
+            team = None
+            if request.POST.get('team-register') == 'new':
+                team_name = request.POST.get('enter-team-name')
+                team = Team(name=team_name)
+                team.save()
+            else:
+                team = form.cleaned_data.get('team')
+
+            Student.objects.create(user=user, team = team)
             login(request, form.save())
-            return redirect("")
+            # return redirect("")
     else:
         form = TeamRegistrationForm()
     return render(request, "users/register.html", { "form": form })
